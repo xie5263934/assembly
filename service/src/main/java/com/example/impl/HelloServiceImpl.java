5ab5caaacaa10c36f5031b9215a18b6e444ee433
jinrun.xie <452081365@qq.com>
2019-06-21 22:40:45 +0800

    修复启动时找不到mybaits的xml文件问题

--- service/src/main/java/com/example/impl/HelloServiceImpl.java
+++ service/src/main/java/com/example/impl/HelloServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.impl;
 
 import com.example.DateUtil;
 import com.example.HelloService;
+import com.example.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -14,6 +16,9 @@ import java.util.Date;
 @Service
 @Slf4j
 public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public String hello(String name) {
--- web/src/main/java/com/example/HelloMain.java
+++ web/src/main/java/com/example/HelloMain.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2019/6/20
  **/
 @SpringBootApplication
+@MapperScan(value = "com.example.mapper")
 public class HelloMain {
     public static void main(String[] args) {
         SpringApplication.run(HelloMain.class, args);
